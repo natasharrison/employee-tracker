877c0d34a1edae0c5d649220ae335e08a1f81b03
natasharrison <natasharrison@gmail.com>
2021-06-06 09:00:40 -0700

    env connection to database

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,16 @@
 const mysql = require('mysql2');
+require('dotenv').config()
+
+console.log(process.env.MYSQL_PASSWORD);
 
 // connect to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: 'process.env.MYSQL_PASSWORD',
+        password: process.env.MYSQL_PASSWORD,
         database: 'company'
-    },
-    console.log('Connected to the company database')
+    }
 );
 
 module.exports = db;
